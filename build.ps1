0a5a68fd80175af47eaf94099614d70f43f415bb
Kamil Adamiec <kmlkamilek@gmail.com>
2024-05-20 23:23:49 +0200

    Update PS script (#52)

--- build.ps1
+++ build.ps1
@@ -3,6 +3,8 @@ emcmake cmake . -B build -G "Unix Makefiles"
 Copy-Item -Path .\roms -Destination .\build -Recurse -Force
 Copy-Item .\src\web\index.html .\build -Force
 Copy-Item .\src\web\index.js .\build -Force
+npm install
+npx tailwindcss -i ./src/web/index.css -o ./build/index.css --minify
 
 cd build
 
